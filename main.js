0898cabba7626ab2f4fdb15e016ad5a4497748f8
gouthamkd <you@example.com>
2017-12-08 00:10:12 +0530

    sample push

---
+++ main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.jsx';
+
+ReactDOM.render(
+<div>
+<App title="React application"/>
+<App title="React Native application application"/>
+</div>
+
+, document.getElementById('app'));
